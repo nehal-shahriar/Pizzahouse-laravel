b0b48ab34edc31acc4062e7790b0dd27f6844993
nehal-shahriar <shahriarkhan.ndc@gmail.com>
2021-07-04 21:55:37 +0600

    Commit 7

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -2,23 +2,26 @@
 
 @section('content')
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <div class="wrapper pizza-details">
-            <h1>Order for {{ $pizza->name }}</h1>
-            <p class="type">Type - {{ $pizza->type }}</p>
-            <p class="base">Base - {{ $pizza->base }}</p>
-            <p class="toppings">Extra Toppings:</p>
-            <ul>
+    <div class="pizza">
+        <img src="/img/pizza.jpg" height="300px" weight="300px" alt="An intimidating leopard.">
+    </div>
+    <div class="wrapper pizza-details">
+        <h1>Order for {{ $pizza->name }}</h1>
+        <p class="type">Type - {{ $pizza->type }}</p>
+        <p class="base">Base - {{ $pizza->base }}</p>
+        <p class="toppings">Extra Toppings:</p>
+        <ul>
             @foreach($pizza->toppings as $topping)
-                <li>{{ $topping }}</li>
+            <li>{{ $topping }}</li>
             @endforeach
-            </ul>
-            <form action="/pizzas/{{ $pizza->id }}" method="POST">
+        </ul>
+        <form action="/pizzas/{{ $pizza->id }}" method="POST">
             @csrf
             @method('DELETE')
             <button>Complete Order</button>
-            </form>
-        </div>
+        </form>
+    </div>
 
-        <a href="/pizzas" class="back">Back to all Pizzas</a>
+    <a href="/pizzas" class="back">Back to all Pizzas</a>
 </div>
 @endsection
